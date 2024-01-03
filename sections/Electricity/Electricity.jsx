397cb52aa33330c7d7be8c1b516de75477d640c1
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2024-01-03 18:51:45 +0200

    added an automatic counter

--- sections/Electricity/Electricity.jsx
+++ sections/Electricity/Electricity.jsx
@@ -1,5 +1,20 @@
-import s from './Electricity.module.css'
+import React, { useState, useEffect } from 'react';
+import s from './Electricity.module.css';
+import formatNumberWithDot from '../../utils/formatNumberWithDot';
+
 export default function Electricity() {
+  const [count, setCount] = useState(1134147814);
+  const finalCount = formatNumberWithDot(count);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(prevCount => prevCount + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []); 
+
+
   return (
     <div className="section-main font-oswald">
         <div className="container-main">
@@ -7,7 +22,7 @@ export default function Electricity() {
                 <div className={s.block__title}>
                     <h2 className='title'>Electricity we produced for all time</h2>
                 </div>
-                <p className={s.block__description}><span className={s.block__coordinates}>1.134.147.814</span><span className={s.block__width}>kWh</span></p>
+                <p className={s.block__description}><span className={s.block__coordinates}>{finalCount}</span><span className={s.block__width}>kWh</span></p>
             </div>
         </div>
     </div>
