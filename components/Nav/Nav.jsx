0e0c753f8c14ee319828828044ebe165ee20ef1c
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2024-01-10 15:18:35 +0200

    fix: fix overlay for nav

--- components/Nav/Nav.jsx
+++ components/Nav/Nav.jsx
@@ -10,30 +10,31 @@ import s from "./Nav.module.css";
 
 export default function Nav({ open, handleClose }) {
   const [anchor, setAnchor] = useState("");
-
-  useEffect(() => {
-    const body = document.body;
-    if (open) {
-      body.classList.add("lock");
-    } else {
-      body.classList.remove("lock");
-    }
-    return () => {
-      body.classList.remove("lock");
-    };
-  }, [open]);
-  useEffect(() => {
-    const hash = window.location.hash;
-    setAnchor(hash.substring(1, hash.length));
-  }, []);
-
-  const handleLinkClick = (label) => {
-    setAnchor(label);
-    handleClose();
+useEffect(() => {
+  const body = document.body;
+  if (open) {
+    body.classList.add("lock");
+  } else {
+    body.classList.remove("lock");
+  }
+  return () => {
+    body.classList.remove("lock");
   };
+}, [open]);
+
+useEffect(() => {
+  const hash = window.location.hash;
+  setAnchor(hash.substring(1, hash.length));
+}, []);
+
+const handleLinkClick = (label) => {
+  setAnchor(label);
+  handleClose();
+};
+ 
  
   return (
-    <div className={open ? `${s.overlay}` : ''}> 
+    <div className={open ? `${s.overlay}` : `visually-hidden ${s.overlay}`}> 
     <aside
       className={
         open ? `${s.sectionSidebar}${s.active}` : `${s.sectionSidebar}`
@@ -58,7 +59,7 @@ export default function Nav({ open, handleClose }) {
                   ${
                     (index === 0 && !anchor) || anchor === label ? s.active : ''
                   }`}
-                  onClick={() => handleLinkClick(label)}
+                  onClick={() =>  handleLinkClick(label)}
                 >
                   {label}
                     <GoArrowUpRight size={16} className='group-hover:text-white cursor-pointer ease-in duration-500 '/>
@@ -70,6 +71,6 @@ export default function Nav({ open, handleClose }) {
         <Sociables />
       </div>
     </aside>
-   </div>
+    </div>
   );
 }
