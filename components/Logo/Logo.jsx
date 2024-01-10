1a1e28232a6116c9201ddba11e980a7c05aa2a5e
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2024-01-10 16:12:22 +0200

    feat: add favicon

--- components/Logo/Logo.jsx
+++ components/Logo/Logo.jsx
@@ -6,32 +6,26 @@ import Ecososution from '../../public/assets/icons/ecosolution.svg';
 
 export default function Logo() {
   return (
-  <Link href="/" target='_blank' rel="noopener noreferrer nofollow" role="navigation" aria-label="Main" className="flex flex-row gap-x-1 items-end">
+  <Link href="/" target='_blank' rel="noopener noreferrer nofollow" role="navigation" aria-label="Main" className="flex flex-row gap-x-1 items-center">
       <Image
         src={LogoIcon}
         alt="Logo icon"
-        width={31}
-        height={18}
         priority={true}
-        // className="block w-[31px] h-[18px] desktop:w-auto"
+        className="w-[31px] h-[18px]"
       /> 
       <Image 
         src={Ecososution}
         alt="Ecososution"
-        width={170}
-        height={40}
-        className="hoverSvg"
         priority={true}
-      //  className="hoverSvg block w-[170px] h-[40px] desktop:w-auto"
+        className="hoverSvg w-[170px] h-[40px]"
       />
       <Image 
         src={LogoText}
         alt="GREENERGY FOR LIFE"
-        width={60}
-        height={18}
         priority={true}
-        // className="block w-[60px] h-[18px] desktop:w-auto"
+        className = "w-[60px] h-[18px]"
       />
     </Link>
   )
 }
+// desktop:w-auto
