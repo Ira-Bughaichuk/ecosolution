7eb0135655f8402c5cd4bcf3167dd128c7ed6866
Ira-Bughaichuk <18IraBughaichuk@ukr.net>
2024-01-10 15:34:58 +0200

    feat: add dynamic import of component

--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
 'use client';
-
-import Cases from "@/sections/Cases/Cases";
-import About from "@/sections/About/About";
-import ContactUs from "@/sections/ContactUs/ContactUs";
-import FAQ from "@/sections/FAQ/FAQ";
-import Header from "@/sections/Header/Header";
-import Main from "@/sections/Main/Main";
-import Electricity from "@/sections/Electricity/Electricity";
-import Footer from "@/sections/Footer/Footer";
+import dynamic from "next/dynamic";
+const Cases = dynamic(() => import("../sections/Cases/Cases"))
+const About = dynamic(() => import("../sections/About/About"))
+const ContactUs = dynamic(() => import( "../sections/ContactUs/ContactUs"))
+const FAQ = dynamic(() => import("../sections/FAQ/FAQ"))
+const Header = dynamic(() => import("../sections/Header/Header"))
+const Main = dynamic(() => import("../sections/Main/Main"))
+const Electricity = dynamic(() => import("../sections/Electricity/Electricity"))
+const Footer = dynamic(() => import("../sections/Footer/Footer"))
 
 export default function Home() {
   return (
